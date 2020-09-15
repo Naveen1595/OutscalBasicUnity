54bc4d0988c57640e7ef89dfe05c453b0be6596b
Naveen1595 <naveen.rtw1595@gmail.com>
2020-09-15 13:09:00 +0530

    Assets

---
+++ Assets/Scripts/Script01.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Script01 : MonoBehaviour
+{
+    public Button button01;
+    public Button button02;
+    public Button button03;
+
+    public string scene01;
+    public string scene02;
+    public string scene03;
+    public void Start()
+    {
+        button01.onClick.AddListener(onButtonClick01);
+        button02.onClick.AddListener(onButtonClick02);
+        button03.onClick.AddListener(onButtonClick03);
+    }
+    private void onButtonClick01()
+    {
+        Debug.Log("1 Clicked");
+        SceneManager.LoadScene(scene01);
+    }
+    private void onButtonClick02()
+    {
+        Debug.Log("2 Clicked");
+        SceneManager.LoadScene(scene02);
+    }
+    private void onButtonClick03()
+    {
+        Debug.Log("3 Clicked");
+        SceneManager.LoadScene(scene03);
+    }
+}
---
+++ Assets/Scripts/Script02.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Script02 : MonoBehaviour
+{
+    public Button button;
+    public string backScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(onButtonClick);
+    }
+
+    private void onButtonClick()
+    {
+        Debug.Log("Back Clicked");
+        SceneManager.LoadScene(backScene);
+    }
+}
